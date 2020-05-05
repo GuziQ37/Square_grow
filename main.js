23a6fb4cef2599a9159c6f7134dee651461dac14
GuziQ37 <wojciak.kuba94@gmail.com>
2020-05-05 22:38:50 +0200

    implemented js script for cube sizing by scroll

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const square = document.createElement('div');
 document.body.appendChild(square);
 
+let grow = true; // flaga
+
 let size = 100;
 
 
@@ -8,5 +10,25 @@ square.style.width = size + "px";
 square.style.height = size + "px";
 
 window.addEventListener("scroll", function(){
+
+    if(grow == true ){
+        size += 5;
+        square.style.height = size + "px";
+        square.style.width = size + "px";
+    } else {
+        size -= 5;
+        square.style.height = size + "px";
+        square.style.width = size + "px";
+    }
+    
+    if(size >= window.innerWidth / 2){
+        grow = false;
+    }
+
+    if(size <= window.innerWidth / 8){
+        grow = true;
+    }
+
+
 
 })
